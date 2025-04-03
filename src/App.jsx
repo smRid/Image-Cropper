1c8476de74fe29106b28fd88551faaf8576def88
smRid <omorsk911@gmail.com>
2025-04-04 02:02:10 +0600

    DropZone part update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { DropZone } from "./components";
 
 
 function App() {
 
   return (
     <>
-      <h1>My React App</h1>
+      <div className="relative min-h-screen flex flex-col justify-center items-center p-2 bg-white-50 font-poppins">
+        <DropZone />
+      </div>
     </>
   )
 }
