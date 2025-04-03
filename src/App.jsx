99c4b27ae53e9edc7b0f120677067404f2cb2e56
smRid <omorsk911@gmail.com>
2025-04-04 02:19:01 +0600

    fileReader part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { DropZone } from "./components";
+import  {useFileReader} from "./hooks/useFileReader";
 
 
 function App() {
+  const [{result, error, file,}, setFile] = useFileReader({
+    method: "readAsDataURL",
+    accept : ["image/png, image/jpg, image/jpeg"],
+    maxFileSize: 5,
+  });
 
   return (
     <>
       <div className="relative min-h-screen flex flex-col justify-center items-center p-2 bg-white-50 font-poppins">
-        <DropZone />
+        <DropZone setFile={setFile}/>
+        <img src={result} alt="" />
       </div>
     </>
   )
